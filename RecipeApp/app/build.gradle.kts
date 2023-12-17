7167911d9f766db9600952eeae3cf7b6c8d950ba
firas <tayebfiras@gmail.com>
2023-12-17 21:51:49 +0000

    created receipe view model and JSON handler

--- RecipeApp/app/build.gradle.kts
+++ RecipeApp/app/build.gradle.kts
@@ -50,10 +50,10 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:$2.6.2")
-    implementation("com.squareup.retrofit2:retrofit-adapters:2.9.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("io.coil-kt:coil-compose:2.4.0")
-
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
