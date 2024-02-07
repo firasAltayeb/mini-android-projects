8accca205b4739d83410e9f581545d607733a637
firas <tayebfiras@gmail.com>
2024-02-07 14:19:06 +0000

    CupcakeApp - implemented up button logic

--- CupcakeApp/app/src/main/java/com/example/cupcake/ui/OrderViewModel.kt
+++ CupcakeApp/app/src/main/java/com/example/cupcake/ui/OrderViewModel.kt
@@ -64,10 +64,10 @@ class OrderViewModel : ViewModel() {
 
 
     private fun pickupOptions(): List<String> {
-        val dateOptions = mutableListOf<String>()
         val formatter = SimpleDateFormat("E MMM d", Locale.getDefault())
         val calendar = Calendar.getInstance()
         // add current date and the following 3 dates.
+        val dateOptions = mutableListOf<String>()
         repeat(4) {
             dateOptions.add(formatter.format(calendar.time))
             calendar.add(Calendar.DATE, 1)
