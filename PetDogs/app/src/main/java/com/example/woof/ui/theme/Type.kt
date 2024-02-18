1375f934232df28e519a79b38d89dd5a63830014
firas <tayebfiras@gmail.com>
2024-02-18 00:00:35 +0000

    general code refactoring - removed unnecessary comments

--- PetDogs/app/src/main/java/com/example/woof/ui/theme/Type.kt
+++ PetDogs/app/src/main/java/com/example/woof/ui/theme/Type.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.woof.R
 
-val AbrilFatface = FontFamily(
+val AprilFatFace = FontFamily(
     Font(R.font.abril_fatface_regular)
 )
 
@@ -20,7 +20,7 @@ val Montserrat = FontFamily(
 // Set of Material typography styles to start with
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = AbrilFatface,
+        fontFamily = AprilFatFace,
         fontWeight = FontWeight.Normal,
         fontSize = 36.sp
     ),
